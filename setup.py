05450ce605d17627a7d7a953f92725cd01378a94
PhilipMo <zeroworm@outlook.kr>
2016-09-07 12:45:54 +0900

    update
    swp file

--- setup.py
+++ setup.py
@@ -20,10 +20,10 @@ ________    _________________________________________
 		def inicio1():
 			while True:
 				print ('''
-1) Add Kali repositories & Update 
+1) Add OSPTF Repositories 
 2) View Categories
 3) Install classicmenu indicator
-4) Install Kali menu
+4) Install OSPTF menu
 5) Help
 
 			''')
@@ -32,23 +32,23 @@ ________    _________________________________________
 			
 				while opcion0 == "1":
 					print ('''
-1) Add kali linux repositories
+1) Add OSPTF linux repositories
 2) Update
-3) Remove all kali linux repositories
+3) Remove all OSPTF linux repositories
 4) View the contents of sources.list file
 
 					''')
 					repo = raw_input("\033[1;32mWhat do you want to do ?> \033[1;m")
 					if repo == "1":
 						cmd1 = os.system("apt-key adv --keyserver pgp.mit.edu --recv-keys ED444FF07D8D0BF6")
-						cmd2 = os.system("echo '# Kali linux repositories | Added by Katoolin\ndeb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list")
+						cmd2 = os.system("echo '# OSPTF linux repositories | Added by Katoolin\ndeb http://http.OSPTF.org/OSPTF OSPTF-rolling main contrib non-free' >> /etc/apt/sources.list")
 					elif repo == "2":
 						cmd3 = os.system("apt-get update -m")
 					elif repo == "3":
 						infile = "/etc/apt/sources.list"
 						outfile = "/etc/apt/sources.list"
 
-						delete_list = ["# Kali linux repositories | Added by Katoolin\n", "deb http://http.kali.org/kali kali-rolling main contrib non-free\n"]
+						delete_list = ["# OSPTF linux repositories | Added by Katoolin\n", "deb http://http.OSPTF.org/OSPTF OSPTF-rolling main contrib non-free\n"]
 						fin = open(infile)
 						os.remove("/etc/apt/sources.list")
 						fout = open(outfile, "w+")
@@ -58,7 +58,7 @@ ________    _________________________________________
 						    fout.write(line)
 						fin.close()
 						fout.close()
-						print ("\033[1;31m\nAll kali linux repositories have been deleted !\n\033[1;m")
+						print ("\033[1;31m\nAll OSPTF linux repositories have been deleted !\n\033[1;m")
 					elif repo == "back":
 						inicio1()
 					elif repo == "gohome":
@@ -89,9 +89,9 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 						cmd = os.system("sudo apt-get install classicmenu-indicator")
 
 				elif opcion0 == "4"	:
-					repo = raw_input("\033[1;32mDo you want to install Kali menu ? [y/n]> \033[1;m")
+					repo = raw_input("\033[1;32mDo you want to install OSPTF menu ? [y/n]> \033[1;m")
 					if repo == "y":
-						cmd1 = os.system("apt-get install kali-menu")
+						cmd1 = os.system("apt-get install OSPTF-menu")
 				elif opcion0 == "5":
 					print (''' 
 ****************** +Commands+ ******************
@@ -120,7 +120,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 0) All
 
 			 ''')
-						print ("\033[1;32mSelect a category or press (0) to install all Kali linux tools .\n\033[1;m")
+						print ("\033[1;32mSelect a category or press (0) to install all OSPTF linux tools .\n\033[1;m")
 
 						opcion1 = raw_input("\033[1;36mkat > \033[1;m")
 						if opcion1 == "back":
@@ -128,7 +128,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 						elif opcion1 == "gohome":
 							inicio1()
 						elif opcion1 == "0":
-							cmd = os.system("apt-get -f install acccheck ace-voip amap automater braa casefile cdpsnarf cisco-torch cookie-cadger copy-router-config dmitry dnmap dnsenum dnsmap dnsrecon dnstracer dnswalk dotdotpwn enum4linux enumiax exploitdb fierce firewalk fragroute fragrouter ghost-phisher golismero goofile lbd maltego-teeth masscan metagoofil miranda nmap p0f parsero recon-ng set smtp-user-enum snmpcheck sslcaudit sslsplit sslstrip sslyze thc-ipv6 theharvester tlssled twofi urlcrazy wireshark wol-e xplico ismtp intrace hping3 bbqsql bed cisco-auditing-tool cisco-global-exploiter cisco-ocs cisco-torch copy-router-config doona dotdotpwn greenbone-security-assistant hexorbase jsql lynis nmap ohrwurm openvas-cli openvas-manager openvas-scanner oscanner powerfuzzer sfuzz sidguesser siparmyknife sqlmap sqlninja sqlsus thc-ipv6 tnscmd10g unix-privesc-check yersinia aircrack-ng asleap bluelog blueranger bluesnarfer bully cowpatty crackle eapmd5pass fern-wifi-cracker ghost-phisher giskismet gqrx kalibrate-rtl killerbee kismet mdk3 mfcuk mfoc mfterm multimon-ng pixiewps reaver redfang spooftooph wifi-honey wifitap wifite apache-users arachni bbqsql blindelephant burpsuite cutycapt davtest deblaze dirb dirbuster fimap funkload grabber jboss-autopwn joomscan jsql maltego-teeth padbuster paros parsero plecost powerfuzzer proxystrike recon-ng skipfish sqlmap sqlninja sqlsus ua-tester uniscan vega w3af webscarab websploit wfuzz wpscan xsser zaproxy burpsuite dnschef fiked hamster-sidejack hexinject iaxflood inviteflood ismtp mitmproxy ohrwurm protos-sip rebind responder rtpbreak rtpinsertsound rtpmixsound sctpscan siparmyknife sipp sipvicious sniffjoke sslsplit sslstrip thc-ipv6 voiphopper webscarab wifi-honey wireshark xspy yersinia zaproxy cryptcat cymothoa dbd dns2tcp http-tunnel httptunnel intersect nishang polenum powersploit pwnat ridenum sbd u3-pwn webshells weevely casefile cutycapt dos2unix dradis keepnote magictree metagoofil nipper-ng pipal armitage backdoor-factory cisco-auditing-tool cisco-global-exploiter cisco-ocs cisco-torch crackle jboss-autopwn linux-exploit-suggester maltego-teeth set shellnoob sqlmap thc-ipv6 yersinia beef-xss binwalk bulk-extractor chntpw cuckoo dc3dd ddrescue dumpzilla extundelete foremost galleta guymager iphone-backup-analyzer p0f pdf-parser pdfid pdgmail peepdf volatility xplico dhcpig funkload iaxflood inviteflood ipv6-toolkit mdk3 reaver rtpflood slowhttptest t50 termineter thc-ipv6 thc-ssl-dos acccheck burpsuite cewl chntpw cisco-auditing-tool cmospwd creddump crunch findmyhash gpp-decrypt hash-identifier hexorbase john johnny keimpx maltego-teeth maskprocessor multiforcer ncrack oclgausscrack pack patator polenum rainbowcrack rcracki-mt rsmangler statsprocessor thc-pptp-bruter truecrack webscarab wordlists zaproxy apktool dex2jar python-distorm3 edb-debugger jad javasnoop jd ollydbg smali valgrind yara android-sdk apktool arduino dex2jar sakis3g smali && wget http://www.morningstarsecurity.com/downloads/bing-ip2hosts-0.4.tar.gz && tar -xzvf bing-ip2hosts-0.4.tar.gz && cp bing-ip2hosts-0.4/bing-ip2hosts /usr/local/bin/")	
+							cmd = os.system("apt-get -f install acccheck ace-voip amap automater braa casefile cdpsnarf cisco-torch cookie-cadger copy-router-config dmitry dnmap dnsenum dnsmap dnsrecon dnstracer dnswalk dotdotpwn enum4linux enumiax exploitdb fierce firewalk fragroute fragrouter ghost-phisher golismero goofile lbd maltego-teeth masscan metagoofil miranda nmap p0f parsero recon-ng set smtp-user-enum snmpcheck sslcaudit sslsplit sslstrip sslyze thc-ipv6 theharvester tlssled twofi urlcrazy wireshark wol-e xplico ismtp intrace hping3 bbqsql bed cisco-auditing-tool cisco-global-exploiter cisco-ocs cisco-torch copy-router-config doona dotdotpwn greenbone-security-assistant hexorbase jsql lynis nmap ohrwurm openvas-cli openvas-manager openvas-scanner oscanner powerfuzzer sfuzz sidguesser siparmyknife sqlmap sqlninja sqlsus thc-ipv6 tnscmd10g unix-privesc-check yersinia aircrack-ng asleap bluelog blueranger bluesnarfer bully cowpatty crackle eapmd5pass fern-wifi-cracker ghost-phisher giskismet gqrx OSPTFbrate-rtl killerbee kismet mdk3 mfcuk mfoc mfterm multimon-ng pixiewps reaver redfang spooftooph wifi-honey wifitap wifite apache-users arachni bbqsql blindelephant burpsuite cutycapt davtest deblaze dirb dirbuster fimap funkload grabber jboss-autopwn joomscan jsql maltego-teeth padbuster paros parsero plecost powerfuzzer proxystrike recon-ng skipfish sqlmap sqlninja sqlsus ua-tester uniscan vega w3af webscarab websploit wfuzz wpscan xsser zaproxy burpsuite dnschef fiked hamster-sidejack hexinject iaxflood inviteflood ismtp mitmproxy ohrwurm protos-sip rebind responder rtpbreak rtpinsertsound rtpmixsound sctpscan siparmyknife sipp sipvicious sniffjoke sslsplit sslstrip thc-ipv6 voiphopper webscarab wifi-honey wireshark xspy yersinia zaproxy cryptcat cymothoa dbd dns2tcp http-tunnel httptunnel intersect nishang polenum powersploit pwnat ridenum sbd u3-pwn webshells weevely casefile cutycapt dos2unix dradis keepnote magictree metagoofil nipper-ng pipal armitage backdoor-factory cisco-auditing-tool cisco-global-exploiter cisco-ocs cisco-torch crackle jboss-autopwn linux-exploit-suggester maltego-teeth set shellnoob sqlmap thc-ipv6 yersinia beef-xss binwalk bulk-extractor chntpw cuckoo dc3dd ddrescue dumpzilla extundelete foremost galleta guymager iphone-backup-analyzer p0f pdf-parser pdfid pdgmail peepdf volatility xplico dhcpig funkload iaxflood inviteflood ipv6-toolkit mdk3 reaver rtpflood slowhttptest t50 termineter thc-ipv6 thc-ssl-dos acccheck burpsuite cewl chntpw cisco-auditing-tool cmospwd creddump crunch findmyhash gpp-decrypt hash-identifier hexorbase john johnny keimpx maltego-teeth maskprocessor multiforcer ncrack oclgausscrack pack patator polenum rainbowcrack rcracki-mt rsmangler statsprocessor thc-pptp-bruter truecrack webscarab wordlists zaproxy apktool dex2jar python-distorm3 edb-debugger jad javasnoop jd ollydbg smali valgrind yara android-sdk apktool arduino dex2jar sakis3g smali && wget http://www.morningstarsecurity.com/downloads/bing-ip2hosts-0.4.tar.gz && tar -xzvf bing-ip2hosts-0.4.tar.gz && cp bing-ip2hosts-0.4/bing-ip2hosts /usr/local/bin/")	
 						while opcion1 == "1":
 							print ('''
 \033[1;36m=+[ Information Gathering\033[1;m
@@ -350,7 +350,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 							elif opcion2 == "12":
 								cmd = os.system("apt-get install greenbone-security-assistant")
 							elif opcion2 == "13":
-								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/gsd.git")
+								cmd = os.system("apt-get install git && git clone git://git.OSPTF.org/packages/gsd.git")
 							elif opcion2 == "14":
 								cmd = os.system("apt-get install hexorbase")
 							elif opcion2 == "15":
@@ -408,7 +408,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 							print ('''
 		\033[1;36m=+[ Wireless Attacks\033[1;m
 
- 1) Aircrack-ng				17) kalibrate-rtl
+ 1) Aircrack-ng				17) OSPTFbrate-rtl
  2) Asleap				18) KillerBee
  3) Bluelog				19) Kismet
  4) BlueMaho				20) mdk3
@@ -438,9 +438,9 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 							elif opcion2 == "3":
 								cmd = os.system("apt-get install bluelog")
 							elif opcion2 == "4":
-								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/bluemaho.git")
+								cmd = os.system("apt-get install git && git clone git://git.OSPTF.org/packages/bluemaho.git")
 							elif opcion2 == "5":
-								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/bluepot.git")
+								cmd = os.system("apt-get install git && git clone git://git.OSPTF.org/packages/bluepot.git")
 							elif opcion2 == "6":
 								cmd = os.system("apt-get install blueranger")
 							elif opcion2 == "7":
@@ -460,9 +460,9 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 							elif opcion2 == "14":
 								cmd = os.system("apt-get install giskismet")
 							elif opcion2 == "16":
-								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/gr-scan.git")
+								cmd = os.system("apt-get install git && git clone git://git.OSPTF.org/packages/gr-scan.git")
 							elif opcion2 == "17":
-								cmd = os.system("apt-get install kalibrate-rtl")
+								cmd = os.system("apt-get install OSPTFbrate-rtl")
 							elif opcion2 == "18":
 								cmd = os.system("apt-get install killerbee")
 							elif opcion2 == "19":
@@ -494,7 +494,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 							elif opcion2 == "32":
 								cmd = os.system("apt-get install wifite")
 							elif opcion2 == "0":
-								cmd = os.system("apt-get install -y aircrack-ng asleap bluelog blueranger bluesnarfer bully cowpatty crackle eapmd5pass fern-wifi-cracker ghost-phisher giskismet gqrx kalibrate-rtl killerbee kismet mdk3 mfcuk mfoc mfterm multimon-ng pixiewps reaver redfang spooftooph wifi-honey wifitap wifite")
+								cmd = os.system("apt-get install -y aircrack-ng asleap bluelog blueranger bluesnarfer bully cowpatty crackle eapmd5pass fern-wifi-cracker ghost-phisher giskismet gqrx OSPTFbrate-rtl killerbee kismet mdk3 mfcuk mfoc mfterm multimon-ng pixiewps reaver redfang spooftooph wifi-honey wifitap wifite")
 							elif opcion2 == "back":
 								inicio()
 							elif opcion2 == "gohome":
@@ -607,7 +607,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 							elif opcion2 == "35":
 								print ("Webshag is unavailable")
 							elif opcion2 == "36":
-								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/webslayer.git")
+								cmd = os.system("apt-get install git && git clone git://git.OSPTF.org/packages/webslayer.git")
 							elif opcion2 == "37":
 								cmd = os.system("apt-get install websploit")
 							elif opcion2 == "38":
@@ -671,7 +671,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 							elif opcion2 == "8":
 								cmd = os.system("apt-get install ismtp")
 							elif opcion2 == "9":
-								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/isr-evilgrade.git")
+								cmd = os.system("apt-get install git && git clone git://git.OSPTF.org/packages/isr-evilgrade.git")
 							elif opcion2 == "10":
 								cmd = os.system("apt-get install mitmproxy")
 							elif opcion2 == "11":
@@ -947,7 +947,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 								cmd = os.system("apt-get install bulk-extractor")
 
 							elif opcion2 == "3":
-								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/capstone.git")
+								cmd = os.system("apt-get install git && git clone git://git.OSPTF.org/packages/capstone.git")
 							elif opcion2 == "4":
 								cmd = os.system("apt-get install chntpw")
 							elif opcion2 == "5":
@@ -959,7 +959,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 							elif opcion2 == "8":
 								print ('dff is unavailable')
 							elif opcion2 == "9":
-								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/distorm3.git")
+								cmd = os.system("apt-get install git && git clone git://git.OSPTF.org/packages/distorm3.git")
 							elif opcion2 == "10":
 								cmd = os.system("apt-get install dumpzilla")
 							elif opcion2 == "11":
@@ -1029,7 +1029,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 							elif opcion2 == "3":
 								cmd = os.system("apt-get install iaxflood")
 							elif opcion2 == "4":
-								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/inundator.git")
+								cmd = os.system("apt-get install git && git clone git://git.OSPTF.org/packages/inundator.git")
 							elif opcion2 == "5":
 								cmd = os.system("apt-get install inviteflood")
 							elif opcion2 == "6":
@@ -1105,7 +1105,7 @@ For more information , please visit : http://www.florian-diesch.de/software/clas
 							elif opcion2 == "8":
 								cmd = os.system("apt-get install crunch")
 							elif opcion2 == "9":
-								cmd = os.system("apt-get install git && git clone git://git.kali.org/packages/dbpwaudit.git")
+								cmd = os.system("apt-get install git && git clone git://git.OSPTF.org/packages/dbpwaudit.git")
 							elif opcion2 == "10":
 								cmd = os.system("apt-get install findmyhash")
 							elif opcion2 == "11":
